dcdce99d7a4020f568380730e4b31cb5d8e6a379
Alex <asu9@yandex.ru>
2024-02-26 19:34:55 +0600

    поправил ошибку

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App/app';
 import OffersData from './mocks/offers';
 import FavoriteData from './mocks/favorites';
+import App from './App/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
