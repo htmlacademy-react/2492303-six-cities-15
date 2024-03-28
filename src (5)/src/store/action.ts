05f7897cb3f28173d39938ab9e0c25583b7a27d3
Alex <asu9@yandex.ru>
2024-03-28 13:24:56 +0600

    ошибочный коммит

--- src (5)/src/store/action.ts
+++
@@ -1,8 +0,0 @@
-import { AppRoute, TCity, TOffer } from './../const';
-import {createAction} from '@reduxjs/toolkit';
-
-export const updateCity = createAction<TCity>('city/updateCity');
-
-export const insertOffer = createAction<TOffer>('offer/insertOffer');
-
-export const redirectToRoute = createAction<AppRoute>('/six-cities/offers');
